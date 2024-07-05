e2d095fd389c9c2ec69d64c93c83c6b2b2aaaea1
leop171 <leop88567@gmail.com>
2024-07-04 19:42:49 -0600

    insercionFormulario.php, coloque la conexion e insercion a base de datos dentro de un bloque try-catch para mejorar el control de errores

--- php/insercionFormulario.php
+++ php/insercionFormulario.php
@@ -14,53 +14,105 @@ header('Content-Type: application/json');
 $input = file_get_contents("php://input");
 $data = json_decode($input, true);
 
-if ($data) {
-    $correo = $data['correo'] ?? 'No correo';
-    $contrasenia = $data['contrasenia'] ?? 'No contaseña';
-
-    // Limpiar los valores recibidos
-    LimpiarCorreo($correo);
-    LimpiarContrasenia($contrasenia);    
-
-    // Verificar que el correo no existe
-    // $buscarCorreo = $conexion->prepare("SELECT * FROM foro.usuario WHERE correo = :correo");    
-    // $buscarCorreo->bindParam("correo", $correo, PDO::PARAM_STR);
-    // $buscarCorreo->execute();
-
-    // if($buscarCorreo->rowCount() > 0){
-    //     $data['correo'] = "Correo en uso";
-    //     $data['contrasenia'] = "Contrasenia";        
-    // }
-
-    $insertarUsuario = $conexion -> prepare("INSERT INTO foro.usuario (codigo, nombre, correo, contrasenia, foto, descripcion, fecha)
-    VALUES(:clave, :nombre, :correo, :contrasenia, :foto, :descripcion, :fecha);");
-
-    $insertarUsuario -> bindParam(':correo', $correo);
-    $insertarUsuario -> bindParam(':contrasenia', $contrasenia);
-    $insertarUsuario -> bindParam(':clave', $clave);
-    $insertarUsuario -> bindParam(':nombre', $nombre);
-    $insertarUsuario -> bindParam(':foto', $foto);
-    $insertarUsuario -> bindParam(':descripcion', $descripcion);
-    $insertarUsuario -> bindParam(':fecha', $fecha);
-
-    $insertarUsuario -> execute();
-    // echo "Se inserto correctamente";
+try{
+    if ($data) {
+        $correo = $data['correo'] ?? 'No correo';
+        $contrasenia = $data['contrasenia'] ?? 'No contaseña';
     
-    // Enviar una respuesta de vuelta al cliente
-    echo json_encode([
+        // Limpiar los valores recibidos
+        if(!LimpiarCorreo($correo)){
+            throw new Exception("Correo no valido");
+        }
+        elseif(!LimpiarContrasenia($contrasenia)){
+            throw new Exception("Contrasenia poco segura");
+        }    
+    
+        // Verificar que el correo no existe
+        $buscarCorreo = $conexion->prepare("SELECT * FROM foro.usuario WHERE correo = :correo");    
+        $buscarCorreo->bindParam("correo", $correo, PDO::PARAM_STR);
+        $buscarCorreo->execute();
+    
+        if($buscarCorreo->rowCount() > 0){
+            throw new Exception("El correo ya esta en uso");
+        }
+    
+        $insertarUsuario = $conexion -> prepare("INSERT INTO foro.usuario (codigo, nombre, correo, contrasenia, foto, descripcion, fecha)
+        VALUES(:clave, :nombre, :correo, :contrasenia, :foto, :descripcion, :fecha);");
+    
+        $insertarUsuario -> bindParam(':correo', $correo);
+        $insertarUsuario -> bindParam(':contrasenia', $contrasenia);
+        $insertarUsuario -> bindParam(':clave', $clave);
+        $insertarUsuario -> bindParam(':nombre', $nombre);
+        $insertarUsuario -> bindParam(':foto', $foto);
+        $insertarUsuario -> bindParam(':descripcion', $descripcion);
+        $insertarUsuario -> bindParam(':fecha', $fecha);
+    
+        $insertarUsuario -> execute();
+        // echo "Se inserto correctamente";
+        
+        // Enviar una respuesta de vuelta al cliente
+        echo json_encode([
         'status' => 'success',
         'message' => 'Datos recibidos correctamente',
         'correo' => $correo,
         'contrasenia' => $contrasenia
-    ]);
-} else {
+        ]);    
+    }
+}catch(Exception $Error){
     echo json_encode([
-        'status' => 'error',
-        'message' => 'No se recibieron datos'
+        'status' => 'succes',
+        'message' => 'Error',
+        'correo' => $Error ->getMessage(),
+        'contrasenia' => $contrasenia
     ]);
 }
 
 
+// if ($data) {
+//     $correo = $data['correo'] ?? 'No correo';
+//     $contrasenia = $data['contrasenia'] ?? 'No contaseña';
+
+//     // Limpiar los valores recibidos
+//     LimpiarCorreo($correo);
+//     LimpiarContrasenia($contrasenia);    
+
+//     // Verificar que el correo no existe
+//     // $buscarCorreo = $conexion->prepare("SELECT * FROM foro.usuario WHERE correo = :correo");    
+//     // $buscarCorreo->bindParam("correo", $correo, PDO::PARAM_STR);
+//     // $buscarCorreo->execute();
+
+//     // if($buscarCorreo->rowCount() > 0){
+//     //     $data['correo'] = "Correo en uso";
+//     //     $data['contrasenia'] = "Contrasenia";        
+//     // }
+
+//     $insertarUsuario = $conexion -> prepare("INSERT INTO foro.usuario (codigo, nombre, correo, contrasenia, foto, descripcion, fecha)
+//     VALUES(:clave, :nombre, :correo, :contrasenia, :foto, :descripcion, :fecha);");
+
+//     $insertarUsuario -> bindParam(':correo', $correo);
+//     $insertarUsuario -> bindParam(':contrasenia', $contrasenia);
+//     $insertarUsuario -> bindParam(':clave', $clave);
+//     $insertarUsuario -> bindParam(':nombre', $nombre);
+//     $insertarUsuario -> bindParam(':foto', $foto);
+//     $insertarUsuario -> bindParam(':descripcion', $descripcion);
+//     $insertarUsuario -> bindParam(':fecha', $fecha);
+
+//     $insertarUsuario -> execute();
+//     // echo "Se inserto correctamente";
+    
+//     // Enviar una respuesta de vuelta al cliente
+//     echo json_encode([
+//         'status' => 'success',
+//         'message' => 'Datos recibidos correctamente',
+//         'correo' => $correo,
+//         'contrasenia' => $contrasenia
+//     ]);
+// } else {
+//     echo json_encode([
+//         'status' => 'error',
+//         'message' => 'No se recibieron datos'
+//     ]);
+// }
 
 
 // Limpiar contraseña
