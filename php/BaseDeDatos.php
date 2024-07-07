b8b966aefc1c91c7670de387604fe26ef5eee7c0
leop171 <leop88567@gmail.com>
2024-07-07 00:23:36 -0600

    BaseDeDatos.php, corregir el error de conexion a la base de datos y teste la creacion de la base de datos desde este archivo

--- php/BaseDeDatos.php
+++ php/BaseDeDatos.php
@@ -1,70 +1,23 @@
 <?php
-// Crear la base de datos con PDO
-$nombreServidor = "localhost";
-$nombreUsuario = "root";
-$contrasenia = "";
-
-$correo = strval('leo345@gmail.com');
-$contraseniaU = strval('12345');
-$clave = 17;
-$nombre = 'Leonaro1234';
-$foto = "foto";
-$descripcion = "Descripcion";
-$fecha = "2024-01-01";
-
-
 // Crear la base de datos inicial
-// try{
-//     $conexion = new PDO("mysql:host=$nombreServidor; dbname = ForoDeDiscucion", $nombreUsuario, $contrasenia);
-//     $conexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//     echo "Conexion exitosa";
+$nombreUsuario = "root";
+$contraseniaDB = "";
+$nombreServidor = "localhost";
 
-//     $sql = file_get_contents('C:\laragon\www\ForoDeDiscucion\bd\foroBD.sql');
-
-//     $conexion->exec($sql);
-//     echo "Script SQL ejecutado exitosamente.";
-
-//     // Aqui va la ejecucion del script
-
-
-// }catch(PDOException $error){
-//     echo "Coneccion fallida: ". $error -> getMessage();
-// }
-
-// Insertar datos en usuario
 try{
-    $conexion = new PDO("mysql:host=$nombreServidor; dbname =foro", $nombreUsuario, $contrasenia);
+    $conexion = new PDO("mysql:host=$nombreServidor; dbname = foro", $nombreUsuario, $contraseniaDB);
     $conexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Conexion exitosa";
 
-    $insertarUsuario = $conexion -> prepare("INSERT INTO foro.usuario (codigo, nombre, correo, contrasenia, foto, descripcion, fecha)
-    VALUES(:clave, :nombre, :correo, :contrasenia, :foto, :descripcion, :fecha);");
+    $sql = file_get_contents('C:\laragon\www\ForoDeDiscucion\bd\foroBD.sql');
 
-/*
-INSERT INTO foro.usuario (codigo, nombre, correo, contrasenia, foto, descripcion, fecha)
-VALUES('13', 'Leonado1', 'leo123@gmail.com', '1234', 'foto/foto', 'Soy el primer usuario insertado', '2024-01-01')
-*/
-
-    $insertarUsuario -> bindParam(':correo', $correo);
-    $insertarUsuario -> bindParam(':contrasenia', $contraseniaU);
-    $insertarUsuario -> bindParam(':clave', $clave);
-    $insertarUsuario -> bindParam(':nombre', $nombre);
-    $insertarUsuario -> bindParam(':foto', $foto);
-    $insertarUsuario -> bindParam(':descripcion', $descripcion);
-    $insertarUsuario -> bindParam(':fecha', $fecha);
-
-    $insertarUsuario -> execute();
-    echo "Se inserto correctamente";
-
-    //$insertar = file_get_contents('C:\laragon\www\ForoDeDiscucion\bd\isnercion.sql');
-    
-    // if($conexion -> exec($insertarUsuario)){
-    //     echo "Se inserto con exito el usuario";
-    // };
+    $conexion->exec($sql);
+    echo "Script SQL ejecutado exitosamente.";
 
 }catch(PDOException $error){
     echo "Algo salio mal ". $error -> getMessage(); 
 }
+
 
 // Crear la base de datos con PDO
 // $dataBase = "CREATE DATABASE foroDiscucion";
