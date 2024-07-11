d0f767378e0dbc6219e00fc575e98c35ebfdb96a
leop171 <leop88567@gmail.com>
2024-07-10 19:20:43 -0600

    mysql.php, En este archivo se realizaran todas las consultas a la base de datos con PDO MYSQL, cada vez que se necesite realizar una consulta a la base de datos se debe realizar en este script y llamar al archivo donde sea necesario con include

---
+++ php/mysql.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+$codigo = $_SESSION["codigoUsuario"];
+    
+$obtenerUsuario = $conexion->prepare("SELECT codigo, nombre, correo, foto, descripcion, fecha FROM foro.usuario WHERE codigo = :codigo");
+$obtenerUsuario->bindParam("codigo", $codigo, PDO::PARAM_STR);
+$obtenerUsuario->execute();
+
+$resultadoUsuario = $obtenerUsuario->fetch(PDO::FETCH_ASSOC);
+
+$usuarioCodigo = $resultadoUsuario["codigo"];
+$usuarioNombre = $resultadoUsuario["nombre"];
+$usuarioCorreo = $resultadoUsuario["correo"];
+$usuarioFoto = $resultadoUsuario["foto"];
+$usuarioDescripcion = $resultadoUsuario["descripcion"];
+
+
+
+
+?>
+
+
