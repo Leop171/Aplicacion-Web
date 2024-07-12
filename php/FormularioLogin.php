1a982afd8cc11cf3f1e16bb4927f24035742bb0d
leop171 <leop88567@gmail.com>
2024-07-12 00:29:48 -0600

    formularioLogin.php, este archivo contendra la logica de inicio de sesion php, se debe verificar en validarFormulario.js que documento html fur cargado si login o registro y apartir de eso crear un funcion que maneje cada caso, la funcion que de login debe enviar hacia este archivo, la funcion de registro debe enviar hacia insercionFormulario.php

---
+++ php/FormularioLogin.php
@@ -0,0 +1,83 @@
+<?php
+include __DIR__. "/config.php";
+session_start();
+
+header('Content-Type: application/json');
+
+// Leer los datos JSON del cuerpo de la solicitud
+$input = file_get_contents("php://input");
+$data = json_decode($input, true);
+
+try{
+    if ($data) {
+        $correo = $data['correo'] ?? 'No correo';
+        $contrasenia = $data['contrasenia'] ?? 'No contaseña';
+    
+        // Limpiar los valores recibidos
+        if(!LimpiarCorreo($correo)){
+            throw new Exception("Correo no valido");
+        }
+        elseif(!LimpiarContrasenia($contrasenia)){
+            throw new Exception("Contrasenia poco segura");
+        }    
+    
+        // Verificar que el correo ya esta registrado
+        $buscarCorreo = $conexion->prepare("SELECT * FROM foro.usuario WHERE correo = :correo");    
+        $buscarCorreo->bindParam("correo", $correo, PDO::PARAM_STR);
+        $buscarCorreo->execute();
+    
+        $resultado = $buscarCorreo->fetch(PDO::FETCH_ASSOC);
+
+        if(!$resultado){
+            throw new Exception("Correo no encontrado");
+        }elseif(password_verify($contrasenia, $resultado["contrasenia"])){
+            $_SESSION["codigoUsuario"] = $resultado["contrasenia"];
+        }        
+
+            
+        header('Content-Type: application/json');
+
+        echo json_encode([
+            'status' => 'Accediendo',
+            'message' => 'Accediendo'
+        ]);
+
+        exit();
+
+    }
+}catch(Exception $Error){
+    echo json_encode([
+        'status' => 'Error',
+        'message' => $Error ->getMessage()
+    ]);
+}
+
+
+
+// Limpiar contraseña
+function LimpiarContrasenia($valor){
+    $expresionRegular = "/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{8,}$/";
+    $valor = trim($valor);
+    $valor = stripslashes($valor);
+    $valor = htmlspecialchars($valor);
+
+    if(preg_match($expresionRegular, $valor)){
+        return true;        
+    }else{
+        return false;
+    }
+}
+
+// Limpiar el correo 
+function LimpiarCorreo($valor){
+    $valor = trim($valor);
+    $valor = stripslashes($valor);
+    $valor = htmlspecialchars($valor);
+
+    if(filter_var($valor, FILTER_VALIDATE_EMAIL)){
+        return true;
+    }else{
+        return false;
+    }
+}
+?>
